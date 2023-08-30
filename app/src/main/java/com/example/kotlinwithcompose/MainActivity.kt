168eb9ed179986a5205419fc972711c68893a759
Josed0912 <josephdtowers2@gmail.com>
2023-08-30 14:47:36 -0400

    Vacuous Change

--- app/src/main/java/com/example/kotlinwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/kotlinwithcompose/MainActivity.kt
@@ -36,6 +36,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+
+    //Comment
 }
 
 @Preview(showBackground = true)
